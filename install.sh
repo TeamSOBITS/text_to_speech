337827e97c4bec86ab9d2aa4bc78b9ab8a07c04f
agurakakenai <you@example.com>
2020-10-21 20:58:15 +0900

    added -y

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ pip install gTTS==2.0.1
 pip install gTTS-token --upgrade
 
 echo "Install mpg321"
-sudo apt install mpg321
-  
+sudo apt install mpg321 -y
+
 echo "Install voice data"
-sudo apt install open-jtalk open-jtalk-mecab-naist-jdic hts-voice-nitech-jp-atr503-m001
+sudo apt install open-jtalk open-jtalk-mecab-naist-jdic hts-voice-nitech-jp-atr503-m001 -y
 
 echo "Install wave"
-pip install wave  
+pip install wave
 
 echo "Install mutagen"
 pip install mutagen
