0f434714823dc6ba6f8aea7099622ba4093fb29c
MrKeith99 <kvalentincardenas@hotmail.com>
2022-06-29 02:29:39 +0900

    update install_sh

--- install.sh
+++ install.sh
@@ -1,29 +1,46 @@
 #! /bin/bash
 
+
+echo "╔══╣ Install: Text-to-Speech (STARTING) ╠══╗"
+
+
+sudo apt-get update
+
 echo "Install gTTS"
-pip install gTTS==2.0.1
-pip install gTTS-token --upgrade
+python3 -m pip install gTTS==2.0.1
+python3 -m pip install gTTS-token --upgrade
 
 echo "Install mpg321"
-sudo apt install mpg321 -y
+sudo apt-get install -y \
+    mpg321
 
 echo "Install voice data"
-sudo apt install open-jtalk open-jtalk-mecab-naist-jdic hts-voice-nitech-jp-atr503-m001 -y
+sudo apt install -y \
+    open-jtalk \
+    open-jtalk-mecab-naist-jdic \
+    hts-voice-nitech-jp-atr503-m001
 
 echo "Install wave"
-pip install wave
+python3 -m pip install \
+    wave
 
 echo "Install mutagen"
-pip install mutagen
+python3 -m pip install \
+    mutagen
 
 echo "Install pico2wave"
-sudo apt install libttspico-utils
+sudo apt-get install -y \
+    libttspico-utils
 
 echo "Install alsa"
-sudo apt install alsa alsa-utils
+sudo apt-get install -y \
+    alsa \
+    alsa-utils
 
 echo "Install soundfile"
-sudo pip install soundfile
-sudo apt install -y python-soundfile
+python3 -m pip install soundfile
+sudo apt-get install -y \
+    python-soundfile
 
-echo "finish"
+
+echo "╚══╣ Install: Text-to-Speech (FINISHED) ╠══╝"
